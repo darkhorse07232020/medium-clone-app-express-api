868d55639c828176af5fe6c80248ad9fbd2a2261
Sergej Melihov <darkhorse0723@gmail.com>
2022-11-09 23:51:58 +0900

    Add: Auth API

---
+++ src/utils/validation.js
@@ -0,0 +1,31 @@
+const emailRegexp = new RegExp(
+  /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+);
+
+const validateRegisterParams = (params) => {
+  console.log('params: ', params);
+  const msg = {
+    is_valid: false,
+    error_msgs: [],
+  };
+
+  if (!params) {
+    msg.error_msgs.push('No parameter asigned.');
+    return msg;
+  }
+
+  if (typeof params.email !== 'string' || params.email === '' || !emailRegexp.test(params.email)) msg.error_msgs.push('Invalid email');
+  if (typeof params.password !== 'string' || params.password === '') msg.error_msgs.push('Invalid password');
+  if (typeof params.phone !== 'string' || params.phone === '') msg.error_msgs.push('Invalid phone number');
+  if (typeof params.firstname !== 'string' || params.firstname === '') msg.error_msgs.push('Invalid first name');
+  if (typeof params.lastname !== 'string' || params.lastname === '') msg.error_msgs.push('Invalid last name');
+
+  if (msg.error_msgs.length === 0) {
+    msg.is_valid = true;
+  }
+  return msg;
+};
+
+module.exports = {
+  validateRegisterParams,
+};
